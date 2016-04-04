02e49644bbd3c31f778b200fbe3b1e5a75c6f6a3
bearkub <jacobyoung930@gmail.com>
2016-04-04 15:29:40 -0700

    Added files via upload
    I've written out the functions for the cabinet except for the visit, not sure what we want for it. Also, I could not test on my computer, I'm getting this error that I absolutely cannot figure out:
    Error	1	error C2679: binary '=' : no operator found which takes a right-hand operand of type 'ingredient' (or there is no acceptable conversion)

--- cabinet.cpp
+++ cabinet.cpp
@@ -1,22 +1,49 @@
-#include <iostream>
 #include <string>
 #include <vector>
+#include <iostream>
 #include "cabinet.h"
 
 using namespace std;
 
-void cabinet::addIng(string ingredient) {
-
+cabinet::cabinet()
+{
 }
 
-void cabinet::visitIngredients(ingredient i) {
-
+cabinet::~cabinet()
+{
 }
 
-void cabinet::removeIng(string ingredient) {
-
+void cabinet::addIng(string ing)
+{
+	ingredient i(ing);
+	ingredientList.push_back(i);
 }
 
-void cabinet::clearIngs() {
+void cabinet::visitIngredients(ingredient ing)
+{
+	
+}
+
+void cabinet::removeIng(string ing)
+{
+	ingredient i(ing);
+	for(std::vector<ingredient>::iterator iter = ingredientList.begin(); iter != ingredientList.end(); ++iter) //only works if we restrict cabinet st there are no duplicate Ings
+	{
+		if(*iter == i)
+		{
+			ingredientList.erase(iter);
+			break;
+		}
+	}
+}
+
+void cabinet::clearIngs()
+{
 	ingredientList.clear();
+}
+
+void cabinet::displayCab()
+{
+	std::copy(ingredientList.begin(), ingredientList.end(),
+        std::ostream_iterator<int>(std::cout, " "));
 }
