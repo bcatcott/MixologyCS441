9096f1a552c490145b465e7df97d4db70bee51aa
RajakJamal <jamal002@cougars.csusm.edu>
2016-04-04 22:59:51 -0700

    Update cabinet.cpp

--- cabinet.cpp
+++ cabinet.cpp
@@ -6,7 +6,10 @@
 using namespace std;
 
 void cabinet::addIng(string ingredient) {
-
+	if (searchDatabase(ingredient)  == true)
+	{
+		ingredientList.push_back(ingredient);
+	}
 }
 
 void cabinet::visitIngredients(ingredient i) {
@@ -19,4 +22,19 @@ void cabinet::removeIng(string ingredient) {
 
 void cabinet::clearIngs() {
 	ingredientList.clear();
+}
+
+bool cabinet::searchDatabase(string ingredient)
+{
+	int size = ingredientList.size();
+	
+	for (int i=0; i < size; i++)
+	{
+		if (ingredientList[i] == ingrediant)
+		{
+			return false;
+		}
+	}
+	
+	return true;
 }
