ed7cba63dd531b9828f7e9c4b5a72b0f4e220659
bearkub <jacobyoung930@gmail.com>
2016-04-06 16:37:01 -0700

    Update ingredient.cpp

--- ingredient.cpp
+++ ingredient.cpp
@@ -1,17 +1,43 @@
-#include <vector>
 #include <string>
 #include "ingredient.h"
 
 using namespace std;
 
-ingredient::ingredient(string inputName) {
+ingredient::ingredient(string inputName)
+{
 	name = inputName;
+	quantity = 1;
 }
 
-bool ingredient::operator==(const ingredient& rhs) {
-	if (name == rhs.name)
+ingredient::ingredient(string inputName, int inputQuantity)
+{
+	name = inputName;
+	quantity = inputQuantity;
+}
+
+ingredient::~ingredient()
+{
+}
+
+bool ingredient::operator==(const ingredient &obj)
+{
+	if(name == obj.name)
 		return true;
 	else
 		return false;
-		
+}
+
+string ingredient::getName()
+{
+	return name;
+}
+
+int ingredient::getQuantity()
+{
+	return quantity;
+}
+
+void ingredient::updateQuantity(int amt)
+{
+	quantity += amt;
 }
