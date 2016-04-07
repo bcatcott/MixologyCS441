91c24330a9954adeb292255a27b873f6704db9aa
RajakJamal <jamal002@cougars.csusm.edu>
2016-04-06 18:37:58 -0700

    Create Cocktail.cpp

---
+++ Cocktail.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include "Cocktail.h"
+
+using namespace std;
+
+  Cocktail::Cocktail()
+  {
+  }
+  
+  Cocktail::~Cocktail();
+  {
+  }
+  
+  void Cocktail::printCocktailName()
+  {
+    cout << cocktailName;
+  }
+  
+  void printCocktailRecipe()
+  {
+  }
+  
+  void printRecipe()
+  {
+  }
