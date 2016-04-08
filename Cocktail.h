99b45b178e30f6862d6a7111299a9b2a0fc56bc7
bearkub <jacobyoung930@gmail.com>
2016-04-08 16:23:23 -0700

    needed constructors to be defined

--- Cocktail.h
+++ Cocktail.h
@@ -7,18 +7,15 @@ using namespace std;
 
 class Cocktail
 {
-  public:
-  
-    string cocktailName;
-    vector <Ingredient> recipe;
-    
-  
-  public:
-    
-    Cocktail();
-    ~Cocktail();
-    void printCocktailName();
-    void printCocktailRecipe();
-    void addIngCocktail(Ingredient ing);
+public:
+	string cocktailName;
+	vector<Ingredient> recipe;
 
+public:
+	Cocktail();
+	Cocktail(string inputName);
+	~Cocktail();
+  void printCocktailName();
+  void printCocktailRecipe();
+  void addIngCocktail(Ingredient ing);
 };
