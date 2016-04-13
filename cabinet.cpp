b5883d9a301cf346004c30dbb643fd2f12729529
bearkub <jacobyoung930@gmail.com>
2016-04-13 16:27:05 -0700

    fixed singletons

--- cabinet.cpp
+++ cabinet.cpp
@@ -1,38 +1,50 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include "cabinet.h"
+#include "Cabinet.h"
 
 using namespace std;
 
-cabinet::cabinet()
+Cabinet::Cabinet()
 {
 }
 
-cabinet::~cabinet()
+Cabinet::~Cabinet()
 {
     _instance = 0; //Resets singleton code
 }
 
-void cabinet::addIng(Ingredient ing)
+void Cabinet::addIng(Ingredient ing)
 {
-	string tempName = ing.getName();
-	for (int i = 0; i < tempName.size(); i++)
+	bool pushFlag = false; //checks if an ingredient was pushed onto ingredientList
+	for(size_t j = 0; j < ingredientDBList.size(); j++)
 	{
-		if (tempName[i] == ' ')
-			tempName[i] = '_';
-		else
-			tempName[i] = tolower(tempName[i]);
+		if(ing == ingredientDBList[j]) //compares ingredient trying to add with all ingredients in database
+		{
+			string tempName = ing.getName();
+			for (int i = 0; i < tempName.size(); i++)
+			{
+				if (tempName[i] == ' ')
+					tempName[i] = '_';
+				else
+					tempName[i] = tolower(tempName[i]);
+			}
+			ing.setName(tempName);
+			ingredientList.push_back(ing);
+			pushFlag = true;
+			break;
+		}
 	}
-	ingredientList.push_back(tempName);
+	if(pushFlag = false)
+		cout << "Ingredient not found in database" << endl;
 }
 
-void cabinet::visitIngredients(Ingredient ing)
+void Cabinet::visitIngredients(Ingredient ing)
 {
 
 }
 
-void cabinet::removeIng(Ingredient ing)
+void Cabinet::removeIng(Ingredient ing)
 {
 	for(vector<Ingredient>::iterator iter = ingredientList.begin(); iter != ingredientList.end(); ++iter) //iterates through ingredientList to find matching ingredient to erase
 	{
@@ -44,12 +56,12 @@ void cabinet::removeIng(Ingredient ing)
 	}
 }
 
-void cabinet::clearIngs()
+void Cabinet::clearIngs()
 {
 	ingredientList.clear();
 }
 
-void cabinet::displayCab()
+void Cabinet::displayCab()
 {
 	for (size_t i = 0; i < ingredientList.size(); i++)
 	{
@@ -58,20 +70,19 @@ void cabinet::displayCab()
 	}		
 }
 
-bool cabinet::_instance = 0; //Singleton Code
+Cabinet* Cabinet::_instance = 0; //Singleton Code
 
 
-cabinet cabinet::Instance() //Singleton Code
+Cabinet* Cabinet::Instance() //Singleton Code
 {
 	if (_instance == 0)
 	{
-		_instance = 1;
-		return cabinet();
+		_instance = new Cabinet();
+		return _instance;
 	}
 	else
 	{
-		cout << "Error: can only have one instance of Singleton" << endl;
+		cout << "Error: can only have one instance of Cabinet" << endl;
 		exit(0);
 	}
 }
-
