6ba35c8022190b6c7c827d721d5fa5f7e6c51a1e
bearkub <jacobyoung930@gmail.com>
2016-04-13 16:27:54 -0700

    fixed singleton

--- ingredientdatabase.cpp
+++ ingredientdatabase.cpp
@@ -1,11 +1,10 @@
-#include "ingredientdatabase.h"
-#include "ui_ingredientdatabase.h"
+#include "Ingredientdatabase.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 
-IngredientDatabase::IngredientDatabase(QWidget *parent) :
+/*IngredientDatabase::IngredientDatabase() :
     QMainWindow(parent),
     ui(new Ui::IngredientDatabase)
 {
@@ -13,6 +12,10 @@ IngredientDatabase::IngredientDatabase(QWidget *parent) :
 
     //Create linked-list called IngredientDB
     //Call populateDB() to read file and populate databse
+}*/
+
+IngredientDatabase::IngredientDatabase()
+{
 }
 
 void IngredientDatabase::populateDB(string filename)
@@ -25,7 +28,7 @@ void IngredientDatabase::populateDB(string filename)
       {
         while ( getline (myfile,line) )
         {
-			ingredientList.push_back(Ingredient(line));
+			ingredientDBList.push_back(Ingredient(line));
         }
         myfile.close();
       }
@@ -34,27 +37,34 @@ void IngredientDatabase::populateDB(string filename)
     myfile.close();
 }
 
+void IngredientDatabase::displayDB()
+{
+	for (size_t i = 0; i < ingredientDBList.size(); i++)
+	{
+		ingredientDBList[i].displayIngredient();
+		cout << endl;
+	}
+}
+
 IngredientDatabase::~IngredientDatabase()
 {
-	_instance = 0;
-	delete ui;
+	_instance = 0; //resets Singleton code
 }
 
 
-bool IngredientDatabase::_instance = 0; //Singleton Code
+IngredientDatabase* IngredientDatabase::_instance = 0; //Singleton Code
 
 
-IngredientDatabase IngredientDatabase::Instance(QWidget *parent = 0) //Singleton code (I do not know if it is nessisary to have the QWidget *parent = 0 here or just at the declaration for the constructor)
+IngredientDatabase* IngredientDatabase::Instance() //Singleton code
 {
 	if (_instance == 0)
 	{
-		_instance = 1;
-		return IngredientDatabase(QWidget *parent = 0);
+		_instance = new IngredientDatabase;
+		return _instance;
 	}
 	else
 	{
-		cout << "Error: can only have one instance of ingredientdatabase" << endl;
+		cout << "Error: can only have one instance of IngredientDatabase" << endl;
 		exit(0);
 	}
 }
-
